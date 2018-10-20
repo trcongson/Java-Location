98c8d937b9cd96a48fc94d9baa72cc31c48bb808
trcongson <trcongson.tcs@gmail.com>
2018-10-20 21:22:32 +0700

    2nd cm

--- src/location/Location.java
+++ src/location/Location.java
@@ -1,7 +1,28 @@
 package location;
 
 public class Location {
-    int row, column;
-    double maxValue;
+    private int row;
+    private  int column;
+    private double maxValue;
 
+    public Location(){}
+
+    public Location (int row, int column, double maxValue){
+        this.row = row;
+        this.column = column;
+        this.maxValue = maxValue;
+    }
+
+    public void locateLargest(double[][]a){
+        for (int i=0; i<a.length; i++){
+            for (int j=0;j<a.length;j++){
+                if (maxValue <= a[i][j]){
+                    maxValue = a[i][j];
+                    this.row = i;
+                    this.column = j;
+                }
+            }
+        }
+        System.out.println("Max = " + maxValue + " in row " + (this.row + 1)+ " and column" + (this.column + 1));
+    }
 }
--- src/location/TestLocation.java
+++ src/location/TestLocation.java
@@ -1,12 +1,46 @@
 package location;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TestLocation {
     public static void main(String[] args) {
-        Location location = new Location();
-
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the array: ");
+
+        System.out.println("Nhập số lượng mảng số: ");
+        int quanlity = sc.nextInt();
+
+        System.out.println("Nhập số lượng trong mảng số: ");
+        int lengthArr = sc.nextInt();
+
+        double[][]arrNum = inputArrAll(quanlity, lengthArr);
+        displayArrAll(arrNum);
+
+        Location locationMax = new Location(quanlity, lengthArr, arrNum[0][0]);
+        locationMax.locateLargest(arrNum);
+    }
+
+    private static double[][] inputArrAll(int quanlity, int lengthArr){
+        Random rd = new Random();
+        double[][]arrNum = new double[quanlity][lengthArr];
+        int i =0, j=0;
+        for (i=0;i<quanlity;i++){
+            for (j=0;j<lengthArr;j++){
+                arrNum[i][j] = (double) rd.nextInt(100)*0.5;
+            }
+        }
+        return arrNum;
+    }
+
+    private static void displayArrAll(double[][]arrNum){
+        System.out.println("Mảng số gồm: ArrNum{");
+        for (int i =0;i<arrNum.length;i++){
+            System.out.println("\t{");
+            for (int j =0; j< arrNum[i].length;j++){
+                System.out.print(arrNum[i][j] + " ");
+            }
+            System.out.print("}\n");
+        }
+        System.out.println("}");
     }
 }
